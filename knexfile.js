bb0e9e5985e83e068c1356419fe66f7e9e38d347
danielarecreo <recreodaniela@gmail.com>
2021-09-15 03:06:49 +0800

    Comment table creation for migrations

--- knexfile.js
+++ knexfile.js
@@ -13,9 +13,9 @@ module.exports = {
       min: 2,
       max: 10
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
   }
 
 };
