ce2a9c0d715930a7137bd2a94cb620a1f8601966
danielarecreo <recreodaniela@gmail.com>
2021-09-16 15:49:42 +0800

    Initial changes for routes

--- server.js
+++ server.js
@@ -1,53 +1,14 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
-fastify.route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      name: { type: 'string' }
-    },
-    // the response needs to be an object with an `hello` property of type 'string'
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' }
-        }
-      }
-    }
-  },
-  // this function is executed for every request before the handler is executed
-  preHandler: async (request, reply) => {
-    // E.g. check authentication
-  },
-  handler: async (request, reply) => {
-    return { hello: 'ela' }
-  }
-})
-
-// const fastify = require('fastify')()
-
-fastify.register(require('fastify-postgres'), {
-  connectionString: 'postgres://postgres@localhost/postgres'
-})
+const authors_list = require('./authors')
 
 fastify.get('/authors', (req, reply) => {
-  fastify.pg.connect(onConnect)
+  reply.send(authors_list)
+})
 
-  function onConnect (err, client, release) {
-    if (err) return reply.send(err)
-
-    client.query(
-      'SELECT authorID, firstName, lastName, penName, birthDate FROM authors',
-      function onResult (errs, result) {
-        release()
-        reply.send(err || result)
-      }
-    )
-  }
+fastify.delete('/authors', (req, reply) => {
+  
 })
 
 const start = async () => {
