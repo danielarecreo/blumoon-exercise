bc3dd21dd3787a3df26d3def63d3b01ca3567c3a
danielarecreo <recreodaniela@gmail.com>
2021-09-20 16:01:34 +0800

    Add decorator

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 'use strict'
 
 const fastify = require('fastify')
+const fp = require('fastify-plugin')
 
-function build(opts={}) {
+function build(fp, opts={}) {
   const app = fastify(opts)
+
+  fp.decorate("utility", function() {
+    return "Decorators are super neat";
+});
+
+
     app.get('/', async function (request, reply) {
-        return ('returns a status code of 200')
+        return (`returns a status code of 200 ${this.utility()}` );
     })
   
     return app
 }
   
 module.exports = build
+
+// module.exports = async function build(fastify, opts) {
+//     fastify.decorate("utility", function() {
+//         return "Decorators are super neat";
+//     });
+
+//     fastify.get('/', async function (request, reply) {
+//         return (`returns a status code of 200 ${this.utility()}` );
+//     })
+// }
--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-'use strict'
+// 'use strict'
 
-const server = require('./app')({
-  logger: {
-    level: 'info',
-    prettyPrint: true
-  }
+// const { default: fastify } = require('fastify')
+
+// const server = require('./app')({
+//   logger: {
+//     level: 'info',
+//     // prettyPrint: true
+//   }
+// })
+
+// fastify.register(server)
+
+// server.listen(3000, (err) => {
+//   if (err) {
+//     console.log(err)
+//     process.exit(1)
+//   }
+// })
+
+const fastify = require("fastify")({
+  logger: true,
 })
 
-server.listen(3000, (err, address) => {
-  if (err) {
-    console.log(err)
-    process.exit(1)
+const routes = require("./routes");
+
+fastify.register(routes)
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
   }
-})
+  catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
