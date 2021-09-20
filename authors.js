6fa4f752aebd5899399cfe74992e0a2466212606
danielarecreo <61310461+danielarecreo@users.noreply.github.com>
2021-09-21 00:45:45 +0800

    Fix possible  merge conflict

--- authors.js
+++
@@ -1,7 +0,0 @@
-let authors = [
-    { authorID: '1', firstName: 'Marie', lastName: 'Curie' },
-    { authorID: '2', firstName: 'Meredith', lastName: 'Grey' },
-    { authorID: '3', firstName: 'Shawn', lastName: 'Mendez' }
-]
-
-module.exports = authors
