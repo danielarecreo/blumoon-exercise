e7e433fd972b87058ee3a37a7fda43c2ed80cdab
danielarecreo <61310461+danielarecreo@users.noreply.github.com>
2021-09-21 00:31:49 +0800

    Fix merge conflicts

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { test } = require('tap')
+const build = require('../app')
+
+// const test = async () => {
+test ('requests the "/" route', async t => {
+  const app = build()
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/'
+  })
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+})
+
+test()
