522460b50decdfcd22ff7d59f44f321261477734
danielarecreo <61310461+danielarecreo@users.noreply.github.com>
2021-09-22 00:48:56 +0800

    Add method that creates an author to author table

--- routes.js
+++ routes.js
@@ -6,4 +6,10 @@ module.exports = async function routes (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { test: `returns a status code of 200 ${this.utility()}` };
   });
+
+  const authorController = require('./controller/author');
+
+  //working, with error "Promise may not be fulfilled with 'undefined' when statusCode is not 204"
+  fastify.post('/authors', authorController.createAuthor);
+
 };
