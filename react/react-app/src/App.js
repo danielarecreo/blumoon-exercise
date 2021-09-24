6261f751b112879e166191a803f603c91426d3f1
danielarecreo <61310461+danielarecreo@users.noreply.github.com>
2021-09-25 00:51:03 +0800

    Add homepage and button to show all authors

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+import { Button } from 'antd';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Welcome to Blumoon Exercise!</p>
+        
+        <small>Click the button below to show all the authors in this exercise.</small>
+        <br />
+        <Button type="primary">Show All</Button>
+        
       </header>
     </div>
   );
