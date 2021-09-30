06cf94c3955fc5825ba50bd4622e31216ce977b2
danielarecreo <61310461+danielarecreo@users.noreply.github.com>
2021-10-01 02:24:20 +0800

    Update routes

--- db/db.js
+++ db/db.js
@@ -1,5 +1,20 @@
-const knex = require('knex');
-const knexfile = require('../knexfile');
-
-const db = knex(knexfile.development);
-module.exports = db;
+const fp = require('fastify-plugin')
+const { Client } = require('pg')
+require('dotenv').config() 
+const client = new Client({ 
+    user: 'postgres', 
+    password:process.env.PASSWORD, 
+    host: 'localhost', 
+    port: 5432, 
+    database: 'exercise'
+}) 
+async function dbconnector(fastify, options) { 
+    try { 
+        await client.connect() 
+        console.log("db connected succesfully") 
+        fastify.decorate('db', {client}) 
+    } catch(err) { 
+        console.error(err) 
+    } 
+} 
+module.exports= fp(dbconnector)
